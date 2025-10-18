06499b8c52de0031f9d3b8bd09f445cd23c70b41
Dirza1 <jasper.olthof@xs4all.nl>
2025-10-18 23:01:57 +0200

    inital commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	defer connection.Close()
+	fmt.Println("Connection successfull")
 }
